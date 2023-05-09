8453b468819000c32f032b64901fc583a4f3bb5a
Tom Pansino <2768420+tpansino@users.noreply.github.com>
2023-05-09 15:14:48 -0700

    WIP: Prisma working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [PrismaService, UserService],
 })
 export class AppModule {}
